11ed81da0e83016fd089b3a95b5338eba4ca3169
err0r10 <dimanah10@gmail.com>
2018-01-17 17:06:49 +0300

    prototype function

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+1. Empty password 1
+2. Special symbol (.,?) 1
+4. From 8 symbols to 40 symbols 2
+5. Chars and number 2
+6. Special symbol ($%#) 1
+7. Upper and lower 2
+8. Black list 1
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,59 @@
+import re
 
 
 def get_password_strength(password):
-    pass
+    return empty_password(password) + \
+        check_length_password(password) + \
+        check_letters_password(password) + \
+        check_numbers_password(password) + \
+        check_lower_and_upper(password) + \
+        check_blacklist_password(password) + \
+        symbols_password(password)
+
+
+def empty_password(password):
+    return 1 if password else 0
+
+
+def check_length_password(password):
+    before_eight = 1 if len(password) > 7 else 0
+    after_fourty = 1 if len(password) < 41 else 0
+    return before_eight + after_fourty
+
+
+def check_letters_password(password):
+    return 1 if re.findall(r'[a-zA-Z]+', password) else 0
+
+
+def check_numbers_password(password):
+    return 1 if re.findall(r'\d+', password) else 0
+
+
+def check_lower_and_upper(password):
+    lower_letters = 1 if re.findall(r'[a-z]+', password) else 0
+    upper_letters = 1 if re.findall(r'[A-Z]+', password) else 0
+    return lower_letters + upper_letters
+
+
+def check_blacklist_password(password):
+    blacklist = ["123456", "qwerty", "drop table user", ""d]
+    return 0 if password in blacklist else 1
+
+
+def symbols_password(password):
+    special_symbols = re.compile("[#@$]")
+    find_special_symbols = re.findall(special_symbols, password)
+    find_all_symbols = re.findall(r"\W+", password)
+    if find_special_symbols == find_all_symbols:
+        return 2
+    elif find_special_symbols:
+        return 1
+    else:
+        return 0
 
 
 if __name__ == '__main__':
-    pass
+    password = ''
+    print(
+        get_password_strength(password)
+    )
