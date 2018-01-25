f1e354bde7185fab1e907b7dfae73a77b1953c68
err0r10 <dimanah10@gmail.com>
2018-01-25 17:56:38 +0300

    refactoring all function

--- password_strength.py
+++ password_strength.py
@@ -2,54 +2,40 @@ import re
 
 
 def get_password_strength(password):
-    return empty_password(password) + \
-        check_length_password(password) + \
-        check_letters_password(password) + \
-        check_numbers_password(password) + \
-        check_lower_and_upper(password) + \
-        check_blacklist_password(password) + \
-        symbols_password(password)
+    return
 
 
 def empty_password(password):
-    return 1 if password else 0
+    return len(password) > 0
 
 
 def check_length_password(password):
-    before_eight = 1 if len(password) > 7 else 0
-    after_fourty = 1 if len(password) < 41 else 0
+    min_size_password = 7
+    max_size_password = 41
+    before_eight = len(password) > min_size_password
+    after_fourty = len(password) < max_size_password
     return before_eight + after_fourty
 
 
-def check_letters_password(password):
-    return 1 if re.findall(r'[a-zA-Z]+', password) else 0
-
-
 def check_numbers_password(password):
-    return 1 if re.findall(r'\d+', password) else 0
+    return re.findall(r'\d+', password) > 0
 
 
 def check_lower_and_upper(password):
-    lower_letters = 1 if re.findall(r'[a-z]+', password) else 0
-    upper_letters = 1 if re.findall(r'[A-Z]+', password) else 0
+    lower_letters = re.findall(r'[a-z]+', password) > 0
+    upper_letters = re.findall(r'[A-Z]+', password) > 0
     return lower_letters + upper_letters
 
 
 def check_blacklist_password(password):
-    blacklist = ["123456", "qwerty", "drop table user", ""d]
-    return 0 if password in blacklist else 1
+    blacklist = ["123456", "qwerty", "drop table user", ""]
+    return password in blacklist
 
 
 def symbols_password(password):
     special_symbols = re.compile("[#@$]")
     find_special_symbols = re.findall(special_symbols, password)
     find_all_symbols = re.findall(r"\W+", password)
-    if find_special_symbols == find_all_symbols:
-        return 2
-    elif find_special_symbols:
-        return 1
-    else:
-        return 0
 
 
 if __name__ == '__main__':
